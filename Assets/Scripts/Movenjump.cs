d08b62c4a11e83a9d4eb4eafcaee166026304db0
csklimowski <csklimowski@email.arizona.edu>
2017-03-24 21:07:44 -0700

    movement script

---
+++ Assets/Scripts/Movenjump.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movenjump : MonoBehaviour {
+
+	private Rigidbody2D body;
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			body.velocity += new Vector2 (0, 5);
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			body.velocity = new Vector2 (-2, body.velocity.y);
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
+			body.velocity = new Vector2 (2, body.velocity.y);
+		}
+	}
+}
