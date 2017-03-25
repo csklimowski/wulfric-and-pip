478dce94ea3fb8eb09fab8fdf5536293966df7f9
cwheeler1997 <cwheeler1997@email.arizona.edu>
2017-03-24 23:08:39 -0700

    pip and wulfric scripts added

---
+++ Assets/Scripts/isPip.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class isPip : MonoBehaviour {
+
+    private Rigidbody2D body;
+    // Use this for initialization
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (body.velocity.y == 0 && Input.GetKeyDown(KeyCode.Space))
+        {
+            body.velocity += new Vector2(0, 5);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            body.velocity = new Vector2(-2, body.velocity.y);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            body.velocity = new Vector2(2, body.velocity.y);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        while(col.gameObject.tag == "water")
+        {
+            body.velocity += new Vector2(0, 10);
+        }
+    }
+}
