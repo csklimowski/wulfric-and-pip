287f57d2bc61b664759f35a8586c8b73d3265a5a
cwheeler1997 <cwheeler1997@email.arizona.edu>
2017-03-26 03:19:01 -0700

    Script to progress through levels

---
+++ Assets/Scripts/Exit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Exit : MonoBehaviour {
+    
+    // Use this for initialization
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Pip" || other.gameObject.tag == "Wulfric")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
