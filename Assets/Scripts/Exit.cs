907a4caaed07bafadad8f40efa6c3d0445178255
cwheeler1997 <cwheeler1997@email.arizona.edu>
2017-03-26 09:21:29 -0700

    Levels 2 competed

--- Assets/Scripts/Exit.cs
+++ Assets/Scripts/Exit.cs
@@ -5,18 +5,19 @@ using UnityEngine.SceneManagement;
 
 
 public class Exit : MonoBehaviour {
-    
+
     // Use this for initialization
-    void OnCollisionEnter2D(Collision2D other)
+    void Start()
     {
-        if ((other.gameObject.tag == "Pip" || other.gameObject.tag == "Wulfric"))
+        
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Wulfric")
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
