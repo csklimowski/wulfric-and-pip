ea42749d751174f458efeaa9278ee97846f20266
cwheeler1997 <cwheeler1997@email.arizona.edu>
2017-03-26 03:41:59 -0700

    Exit needs work and start level 5 (water level)

--- Assets/Scripts/Exit.cs
+++ Assets/Scripts/Exit.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class Exit : MonoBehaviour {
     
     // Use this for initialization
     void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.tag == "Pip" || other.gameObject.tag == "Wulfric")
+        if ((other.gameObject.tag == "Pip" || other.gameObject.tag == "Wulfric"))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
