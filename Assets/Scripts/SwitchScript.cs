d78147672a171e1ddfa6ab8fa2b1a682dcb2338d
csklimowski <csklimowski@email.arizona.edu>
2017-03-25 20:18:35 -0700

    lever animations

--- Assets/Scripts/SwitchScript.cs
+++ Assets/Scripts/SwitchScript.cs
@@ -5,4 +5,19 @@ using UnityEngine;
 public class SwitchScript : MonoBehaviour {
 
 	public DoorScript door;
+	private Animator animator;
+
+	void Start() {
+		animator = GetComponent<Animator> ();
+	}
+
+	public void Toggle() {
+		if (door.open) {
+			animator.SetBool ("Up", false);
+			door.open = false;
+		} else {
+			animator.SetBool ("Up", true);
+			door.open = true;
+		}
+	}
 }
